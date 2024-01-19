25623064c6292fff897af2733e507d271bb3a49a
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-01-19 14:31:14 -0800

    rewrote in supabase, because i realized i don't trust my hardware

--- commands/liars/count.js
+++ commands/liars/count.js
@@ -1,42 +1,14 @@
 const { SlashCommandBuilder } = require("discord.js");
-//const fs = require("fs");
-//const { CookieDB } = require("cookie-driver");
-//const { db_token } = require("../../config.json");
-const sqlite3 = require("sqlite3").verbose();
-const { lie_counter_uid } = require("../../config.json");
-const { trusted_user_uid } = require("../../config.json");
+const { createClient } = require("@supabase/supabase-js");
+const { lie_counter_uid, trusted_user_uid, supabase_url, supabase_key } = require("../../config.json");
 
-/*
-const cookieDB = new CookieDB(
-    "https://cookiedb.com/api/db",
-    db_token
-)
-*/
-
-const db = new sqlite3.Database("./lies.db", sqlite3.OPEN_READWRITE, (err) => {
-    if (err && err.code == "SQLITE_CANTOPEN") {
-        var newdb = new sqlite3.Database("lies.db", (err) => {
-            if (err) {
-                console.log("Getting error " + err);
-                process.exit(1);
-            }
-            newdb.exec(`
-            CREATE TABLE lies (
-                uid INTEGER PRIMARY KEY NOT NULL,
-                lieCount INTEGER NOT NULL
-            )
-            `);
-        });
-        return;
-    } else if (err) {
-        console.log("Getting error " + err);
-        process.exit(1);
+const supabase = createClient(supabase_url, supabase_key, {
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+        detectSessionInUrl: false
     }
 });
-
-
-
-
 
 
 module.exports = {
@@ -83,31 +55,10 @@ module.exports = {
         let liarid = liar?.id.toString();
         let pingliar = "<@"+liar+">";
         console.log("the liar is "+liarid);
-        
-        /*
-        output = await cookieDB.select("lies", `eq($uid,'${liarid}')`, {
-            maxResults: 1,
-        });
-        */
 
-
-        let liarRow;
-
-        db.get("SELECT * FROM lies WHERE uid = ?", [liarid], (err, row) => {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            liarRow = row;
-            console.log(row);
-        });
         let lieCount;
 
-          
-
         let reply = "This message should never be seen. If the bot responded with this, congratulations!";
-        //let ephemeral = false;
-        // see if this stops the random crashing
         await interaction.deferReply();
 
         console.log(interaction.options.getSubcommand());
@@ -116,33 +67,44 @@ module.exports = {
             if (liarid == lie_counter_uid) {
                 console.log("target is lie counter");
                 reply = "I am infallible.";
-            } else if (liarRow) {
-                console.log("Liar exists, incrementing");
-                db.run("UPDATE lies SET lieCount = lieCount + 1 WHERE uid = ?", [liarid], (err) => {
-                    if (err) {
-                        console.error(err.message);
-                    } else {
-                        console.log("Counter incremented for liar");
-                    }
-                });
-                lieCount = liarRow.lieCount+1;
-                reply = pingliar+" has lied "+lieCount+" times.";
             } else {
-                console.log("Liar does not exist, creating");
-                db.run("INSERT INTO lies (uid, lieCount) VALUES (?, 1)", [liarid], (err) => {
-                    if (err) {
-                        console.error(err.message);
-                    } else {
-                        console.log("New liar entry created");
-                    }
-                });
-                lieCount = 1;
-                reply = pingliar+" has lied "+lieCount+" time.";
+                const { data, error } = await supabase
+                    .from("lies")
+                    .select()
+                    .eq("uid", liarid)
+                    .maybeSingle();
+                if (error) throw error;
+                if (data != null) {
+                    console.log("liar exists, updating");
+                    let newCount = data.liecount+1;
+                    const { error } = await supabase
+                        .from("lies")
+                        .update({ liecount: newCount })
+                        .eq("uid", liarid);
+                    if (error) throw error;
+                    reply = reply = pingliar+" has lied "+newCount+" times.";
+                } else {
+                    console.log("liar does not exist, inserting");
+                    const { error } = await supabase
+                        .from("lies")
+                        .insert({ uid: liarid, liecount: 1 });
+                    if (error) throw error;
+                    lieCount = 1;
+                    reply = reply = pingliar+" has lied "+lieCount+" time.";
+                }
+                
+
             }
             break;
         case "count":
-            if (liarRow) {
-                lieCount = liarRow.liecount;
+            const { data, error } = await supabase
+                .from("lies")
+                .select()
+                .eq("uid", liarid)
+                .maybeSingle();
+            if (error) throw error;
+            if (data) {
+                lieCount = data.liecount;
             } else {
                 lieCount = 0;
             }
@@ -161,20 +123,16 @@ module.exports = {
         case "set":
 
             if (interaction.user.id == trusted_user_uid) {
-                let liecount = interaction.options.getInteger("liecount");
+                let newCount = interaction.options.getInteger("liecount");
                 // Example: Update the lieCount in the database
-                db.run("UPDATE lies SET lieCount = ? WHERE uid = ?", [liecount, liarid], (err) => {
-                    if (err) {
-                        console.error(err.message);
-                    } else {
-                        console.log(`Set the lie count of ${pingliar} to ${liecount}.`);
+                //reply = `Set the lie count of ${pingliar} to ${liecount}.`;
+                const { error } = await supabase
+                    .from("lies")
+                    .upsert({ uid: liarid, liecount: newCount });
+                if (error) throw error;
+                
+                reply = `Set the lie count of ${pingliar} to ${newCount}.`;
 
-                        const reply = `Set the lie count of ${pingliar} to ${liecount}.`;
-                        // Send the reply or handle it as needed
-                        console.log(reply);
-                    }
-                });
-                reply = `Set the lie count of ${pingliar} to ${liecount}.`;
             } else {
                 reply = `I'm afraid I can't do that, <@${interaction.user.id}>.`;
             }
